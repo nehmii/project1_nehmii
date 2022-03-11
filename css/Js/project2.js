655ff914cde041d6997326b3f9752e379b8a8241
Nehmii <guturde26@gmail.com>
2022-03-11 16:58:15 -0500

    this shows changes to a better versoin of the page

---
+++ css/Js/project2.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", function(){
+    document.body.style.background=`rgb(${Math.floor(Math.random()* 255)}
+    ,${Math.floor(Math.random()* 255)},${Math.floor(Math.random()* 255)})`
+})
+
+
